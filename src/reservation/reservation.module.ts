95cf7d55c590af65c285788fb079fefd8e21c003
silverz <liisa9601@naver.com>
2021-09-13 19:37:28 +0900

    reservation module

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ReservationController } from './reservation.controller';
-import { ReservationService } from './reservation.service';
+
 
 @Module({})
 export class ReservationModule {}
