3ada9ef6386e2db177a16538da7ff6991136c43e
ohasis <dhgkdud4002@naver.com>
2021-09-15 21:21:33 +0900

    출입 명부 데이터 삽입

--- src/visited/visited.controller.ts
+++ src/visited/visited.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { VisitedService } from './visited.service';
+import { VisitWriteDto } from "./dto/visit-write.dto";
 
 @Controller('visited')
-export class VisitedController {}
+export class VisitedController {
+    constructor(
+        private visitedService : VisitedService
+    ){}
+
+    @Post('/write')
+    writeList(@Body(ValidationPipe) visitWriteDto : VisitWriteDto):Promise<void>{
+        return this.visitedService.writeList(visitWriteDto);
+    }
+}
